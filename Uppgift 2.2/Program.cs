70e3b0c5ca06da07deef5c72bca19ec713bb5c50
hamadali06 <ahal06003@utb.vaxjo.se>
2023-09-15 10:19:14 +0200

    Add project files.

---
+++ Uppgift 2.2/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+namespace Uppgift2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hur långt hoppade Elin?");
+            string x = Console.ReadLine();
+            int y = Convert.ToInt32(x);
+            Console.Clear();
+            Console.WriteLine("Hur långt hoppade Alma?");
+            string a = Console.ReadLine();
+            int b = Convert.ToInt32(a);
+            int dif = y - b; 
+            Console.Clear();
+            Console.WriteLine("Alma hoppade " + dif + " längre än Elin");
+        }
+    }
+}
